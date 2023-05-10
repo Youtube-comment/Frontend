9acd8576aa7dc03bbf518ef5803b9309048c4d19
zzuzzu0205 <zkakcnd@naver.com>
2023-05-10 19:07:52 +0900

    'navbar'

--- src/tool/menu/menu.js
+++ src/tool/menu/menu.js
@@ -3,28 +3,24 @@ import './menu.css';
 
 function Menu(){
     return(
-        <div className="menu_main">
+        <div className="menu_bar">
             <div className="menu_container">
-                <div className="menu_user">
-                    <div className="menu_icon">
-                        사용자 아이콘 부분분
-                    </div>
-                    <div className="menu_name">
-                        사용자 이름부분
-                    </div>
+                <div className="menu_appname">
+                    유튜브댓글 자동처리
                 </div>
-                <div className="menu_channel">
-                    채널 정보 부분
+                <div className="menu_main">
+                    Main
                 </div>
-                <div className="menu_commnet">
-                    댓글 관리 부분
+                <div className="menu_veidos">
+                    Vedios
                 </div>
-                <div className="menu_help">
-                    도움말 부분
+                <div className="menu_profile">
+                    Profile
                 </div>
-                <div className="menu_contact">
-                    고객센터 부분
+                <div className="menu_islogin">
+                    로그아웃
                 </div>
+                
             </div>
         </div>
     );
