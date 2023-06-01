ab83c17d665dfdea7851dc2bc936c91063637c2b
JunYoung <wnsdud96330@gmail.com>
2023-06-01 22:53:17 +0900

    6.1 풀받기

--- src/tool/Navbar/Navbar.js
+++ src/tool/Navbar/Navbar.js
@@ -1,10 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 import Login from "../login/login";
 import { useSelector } from 'react-redux';
+import { cookie, getCookie } from "../util/Cookie";
+import axios from "axios";
+
 function Navbar() {
   let [bg, setBg] = useState(["active", "", "", ""]);
+  const token = getCookie("access_token");
+  let { channel_id } = useParams();
+    useEffect(() => {
+      userData()
+  }, []);
+  const userData = async () => {
+    try {
+      const get_userData = await axios.get(
+        `${process.env.REACT_APP_URL}/api/get_channel_sb/`,
+        {
+          channel_id: channel_id,
+        },
+        {
+          headers: { Authorization: token },
+        }
+      );
+        const userImage = userData.data.items.map((item) => item);
+        
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   return (
     <div className="Navbar_page">
@@ -23,46 +49,22 @@ function Navbar() {
             </div>
           </div>
           <div className="Navbar_menu">
-            <div className={"Navbar_menu_main " + bg[0]}>
-              <Link
-                to="/"
-                className="Navbar_menu_main_item"
-                onClick={() => {
-                  let bg1 = [...bg];
-                  bg1 = ["active", "", "", ""];
-                  setBg(bg1);
-                }}
-              >
+            <div className="Navbar_menu_main">
+              <Link to="/" className="Navbar_menu_main_item">
                 Main
               </Link>
             </div>
-            <div className={"Navbar_menu_videos " + bg[1]}>
-              <Link
-                to="/videos"
-                className="Navbar_menu_videos_item"
-                onClick={() => {
-                  let bg2 = [...bg];
-                  bg2 = ["", "active", "", ""];
-                  setBg(bg2);
-                }}
-              >
+            <div className= "Navbar_menu_videos">
+              <Link to="/videos" className="Navbar_menu_videos_item">
                 Videos
               </Link>
             </div>
-            <div className={"Navbar_menu_profile " + bg[2]}>
-              <Link
-                to="/profile"
-                className="Navbar_menu_profile_item"
-                onClick={() => {
-                  let bg3 = [...bg];
-                  bg3 = ["", "", "active", ""];
-                  setBg(bg3);
-                }}
-              >
+            <div className= "Navbar_menu_profile">
+              <Link to="/profile" className="Navbar_menu_profile_item">
                 profile
               </Link>
             </div>
-            <div className={"Navbar_menu_logout " + bg[3]}>
+            <div className="Navbar_menu_logout">
               <Link to="/" className="Navbar_menu_logout_item">
                 <Login />
               </Link>
